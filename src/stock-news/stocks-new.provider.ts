d6aaed3c7f7b5ccf1f46b1ee0b076a37890ea954
DeepakSingh260 <alexmha260@gmail.com>
2023-02-27 19:55:04 +0530

    stock news added

--- src/stock-news/stocks-new.provider.ts
+++ src/stock-news/stocks-new.provider.ts
@@ -4,18 +4,22 @@ import axios from 'axios'
 export class StockNewsProvider{
     baseUrl:string
     constructor(){
-        this.baseUrl =  'https://newsapi.org/v2/top-headlines';
+        this.baseUrl =  'https://newsapi.org/v2/everything';
 
     }
     async getNews(){
-
         const response = await axios.get(this.baseUrl ,{params:{
             apiKey : '74b6116ed004423bae8f334bb5031186',
             q : 'Stock'
         }})
-
         return {data: response.data}
-
+    }
+    async getNewsForStock(stock:string){
+        const response = await axios.get(this.baseUrl ,{params:{
+            apiKey : '74b6116ed004423bae8f334bb5031186',
+            q : stock
+        }})
+        return {data: response.data}
     }
     
 }
--- src/stock-news/stocks-news.controller.ts
+++ src/stock-news/stocks-news.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get , Param } from "@nestjs/common";
 import { StockNewsProvider } from "./stocks-new.provider";
 
 @Controller('stock-news')
 export class StockNewsController{
     constructor(private stockNewsProvider:StockNewsProvider){}
-    @Get()
+    @Get('/')
     async getStockNews():Promise<object>{
         const response = await this.stockNewsProvider.getNews()
+        return response
+    }
+    @Get('/:stockSymbol')
+    async getStockNewsForStock(@Param() param):Promise<object>{
+        // console.log(param.stockSymbol)
+        const response = await this.stockNewsProvider.getNewsForStock(param.stockSymbol.toString())
         return response
     }
 }
